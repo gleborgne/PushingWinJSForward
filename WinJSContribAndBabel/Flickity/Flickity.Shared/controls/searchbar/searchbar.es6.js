ce3b7e415b14864460eb7120e8b3909fd2fbed29
gleborgne <gleborgne@mcnext.com>
2015-04-19 23:46:15 +0200

    setup babel for ES6 sample

---
+++ WinJSContribAndBabel/Flickity/Flickity.Shared/controls/searchbar/searchbar.es6.js
@@ -0,0 +1,108 @@
+﻿/// <reference path="../../js/_reference.js" />
+
+(function () {
+    "use strict";
+
+    var SearchBarCtor = WinJS.UI.Pages.define("/controls/searchbar/searchbar.html", {
+    	processed: function (element, options) {
+    		var ctrl = this;
+    		ctrl.mainHeader = document.getElementById('appheader');    		
+    		ctrl.eventTracker.addEvent(ctrl.searchInput, 'focus', ctrl.show.bind(ctrl));    		
+    		ctrl.loadLastSearches();
+
+    		ctrl.recentItemsPromise = Flickity.Api.recent(0, 80).then(function (items) {
+    			ctrl.recentItemsList.itemDataSource = new WinJS.Binding.List(items.photos.photo).dataSource;
+    			ctrl.recentItemsList.oniteminvoked = function (arg) {
+    				arg.detail.itemPromise.then(function (item) {
+    					ctrl.hide();
+    					WinJS.Navigation.navigate('/pages/detail/detail.html', { picture: item.data });
+    				});
+    			};
+    		}, ctrl.recentItemsError.bind(ctrl));
+
+    		ctrl.eventTracker.addEvent(WinJS.Application, 'search', function (arg) {
+    			ctrl.runSearch(arg.term);
+    		});
+    	},
+
+    	recentItemsError: function (err) {
+    		console.error(err);
+    	},
+
+    	loadLastSearches: function () {
+    		var ctrl = this;
+    		var lastSearches = localStorage['lastSearches'];
+    		if (!lastSearches)
+    			lastSearches = [];
+    		else
+    			lastSearches = JSON.parse(lastSearches);
+
+    		ctrl.lastSearches = lastSearches;
+    		ctrl.lastsearchItems.innerHTML = '';
+
+    		lastSearches.forEach(function (s) {
+    			var item = document.createElement('SPAN');
+    			item.className = 'search-term';
+    			item.innerText = s;
+				WinJSContrib.UI.tap(item, function () {
+    				ctrl.runSearch(s);
+    			});
+    			ctrl.lastsearchItems.appendChild(item);
+    		});
+    	},
+
+    	addSearchTerm: function (s) {
+    		var ctrl = this;
+    		while (ctrl.lastSearches.indexOf(s) >= 0) {
+    			var idx = ctrl.lastSearches.indexOf(s);
+    			ctrl.lastSearches.splice(idx, 1);
+    		}
+    		ctrl.lastSearches.splice(0, 0, s);
+    		localStorage['lastSearches'] = JSON.stringify(ctrl.lastSearches.slice(0, 10));
+    	},
+
+    	runSearchFromInput: function () {
+    		var ctrl = this;
+    		ctrl.runSearch(ctrl.searchInput.value);
+    	},
+
+    	runSearch: function (searchTerm) {
+    		var ctrl = this;
+    		if (searchTerm && searchTerm.length > 2) {
+    			ctrl.searchInput.value = searchTerm;
+    			ctrl.element.classList.add('hasSearch');
+    			ctrl.addSearchTerm(searchTerm);
+    			ctrl.searchInput.blur();
+    			ctrl.hide();
+    			setImmediate(function () {
+    				WinJS.Navigation.navigate('/pages/home/home.html', { search: ctrl.searchInput.value });
+    				ctrl.loadLastSearches();
+    			});
+
+    		}
+    	},
+
+    	show: function () {
+    		var ctrl = this;
+    		
+    		ctrl.mainHeader.classList.add('expanded');
+    		setTimeout(function () {
+    			ctrl.recentBlock.style.opacity = '0';
+    			ctrl.recentItemsList.element.style.display = '';
+    			WinJS.UI.Animation.fadeIn(ctrl.recentBlock);
+    		}, 300);
+    	},
+
+    	hide: function () {
+    		var ctrl = this;
+
+    		WinJS.UI.Animation.fadeOut(ctrl.recentBlock).then(function () {
+    			ctrl.recentItemsList.element.style.display = 'none';
+    			ctrl.searchInput.blur();
+    			ctrl.mainHeader.classList.remove('expanded');
+    		});
+    	}
+    });
+
+    WinJS.Namespace.define('Flickity.UI', { SearchBar: SearchBarCtor });
+})();
--- WinJSContribAndBabel/gulpfile.js
+++ WinJSContribAndBabel/gulpfile.js
@@ -8,6 +8,9 @@ var minifycss = require('gulp-minify-css');
 var plumber = require('gulp-plumber');
 var del = require('del');
 var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var babel = require('gulp-babel');
+
 
 var onError = function(err) {
 	notify.onError({
@@ -32,7 +35,33 @@ gulp.task('styles', function() {
 	.pipe(gulp.dest(''));	
 });
 
+function babelPipe(sources) {
+    return gulp.src(['Flickity/**/*.es6.js', '!Flickity/**/bin/**/*.es6.js', '!Flickity/**/bld/**/*.es6.js'], { base : '.' })
+        .pipe(plumber({errorHandler: onError}))
+		.pipe(sourcemaps.init())
+        .pipe(babel())        
+        .pipe(rename(function(path){
+        	var idx = path.basename.indexOf('.es6');
+        	path.basename = path.basename.substr(0, idx);
+        	path.extname = '.js';
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(''));
+}
+
+gulp.task('transpilescripts', function () {
+    return babelPipe(['Flickity/**/*.es6.js', '!Flickity/**/bin/**/*.es6.js', '!Flickity/**/bld/**/*.es6.js']);
+});
+
+
 gulp.task('watch', function() {
+	gulp.watch(
+		['Flickity/**/*.es6.js', '!Flickity/**/bin/**/*.es6.js', '!Flickity/**/bld/**/*.es6.js'], 
+		function(vinyl){
+			console.log('babel transpile ' + vinyl.path);
+			return babelPipe(vinyl.path);
+		}
+	);	
 	gulp.watch(['Flickity/**/*.less', '!Flickity/**/bin/**/*.less', '!Flickity/**/bld/**/*.less'], ['styles']);	
 });
 
