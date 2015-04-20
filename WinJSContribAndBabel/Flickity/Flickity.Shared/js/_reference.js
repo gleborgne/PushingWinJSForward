08894cccd2f610bfe5cf619408b7a56b97e07295
gleborgne <gleborgne@mcnext.com>
2015-04-20 11:40:10 +0200

    hide babel sample

--- WinJSContribAndBabel/Flickity/Flickity.Shared/js/_reference.js
+++
@@ -1,5 +0,0 @@
-﻿/// <reference path="flickrapi.js" />
-/// <reference path="Flickity.config.js" />
-/// <reference path="../scripts/WinJS/js/WinJS.js" />
-/// <reference path="../scripts/winjscontrib/winjscontrib.core.js" />
-/// <reference path="../scripts/winjscontrib/winjscontrib.ui.animation.js" />
